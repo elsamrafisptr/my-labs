58e54c43bc30c43b1f08c5e4676d7b2ee2792967
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2025-09-21 13:41:45 +0700

    feat: add some api and api route

---
+++ src/components/elements/check-location.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { useEffect } from 'react'
+
+const CheckLocation = () => {
+  useEffect(() => {
+    if (!localStorage.getItem('location')) {
+      fetch('/api/location')
+        .then(res => res.json())
+        .then(data => localStorage.setItem('location', data.loc))
+    }
+  }, [])
+  return null
+}
+
+export default CheckLocation
--- src/components/elements/navbar.tsx
+++ src/components/elements/navbar.tsx
@@ -1,40 +1,60 @@
+'use client'
+
+import Image from 'next/image'
 import Link from 'next/link'
 
-const navItems = {
-  '/': {
-    name: 'home'
-  },
-  '/blog': {
-    name: 'blog'
-  },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy'
-  }
-}
+import { motion } from 'framer-motion'
+import { MoonIcon, SunIcon } from 'lucide-react'
+import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
-export function Navbar() {
+export const Navbar = () => {
+  const { theme, setTheme } = useTheme()
+  const [isClient, setIsClient] = useState(false)
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
   return (
-    <aside className="mb-16 -ml-[8px] tracking-tight">
-      <div className="lg:sticky lg:top-20">
-        <nav
-          className="fade relative flex scroll-pr-6 flex-row items-start px-0 pb-0 md:relative md:overflow-auto"
-          id="nav"
-        >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="relative m-1 flex px-2 py-1 align-middle transition-all hover:text-neutral-800 dark:hover:text-neutral-200"
-                >
-                  {name}
-                </Link>
-              )
-            })}
-          </div>
-        </nav>
+    <motion.div
+      initial={{ opacity: 0, y: 0 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.2, ease: 'easeInOut' }}
+      className="container my-6 flex max-w-[680px] flex-row items-center justify-between gap-4 lg:my-10"
+    >
+      <div className="flex flex-row items-center gap-4 tracking-tight md:gap-6">
+        <Link href="/" className="hover:opacity-70">
+          <Image
+            src="/vercel.svg"
+            alt="logo"
+            width={32}
+            height={32}
+            className="rounded-full"
+          />
+        </Link>
+        <Link href="/" className="hover:opacity-70">
+          home
+        </Link>
+        <Link href="/blog" className="hover:opacity-70">
+          blog
+        </Link>
+        <Link href="/contact" className="hover:opacity-70">
+          contact
+        </Link>
       </div>
-    </aside>
+      <div className="flex flex-row items-center gap-4 md:gap-6">
+        {isClient ? (
+          <button
+            type="button"
+            onClick={() => {
+              setTheme(theme === 'light' ? 'dark' : 'light')
+            }}
+            className="transition-all hover:opacity-70"
+          >
+            {theme === 'dark' ? <SunIcon size={16} /> : <MoonIcon size={16} />}
+          </button>
+        ) : null}
+      </div>
+    </motion.div>
   )
 }
