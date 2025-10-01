26dfa6d0f3ea5132afb92bac40b6966b834838be
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2025-10-01 21:17:42 +0700

    fix

--- src/modules/home/sections/home.tsx
+++ src/modules/home/sections/home.tsx
@@ -1,8 +1,11 @@
 import { resume } from '@/common/constants'
-import { BlogPosts } from '@/components/elements/posts'
+import BlogCard from '@/components/elements/blog-card'
 import ProjectCard from '@/components/elements/project-card'
+import { formatDate, getBlogPosts } from '@/lib/utils'
 
 const Home = () => {
+  const allBlogs = getBlogPosts()
+
   return (
     <main className="flex flex-col gap-6">
       <section>
@@ -40,7 +43,28 @@ const Home = () => {
         <h3 className="text-md mb-6 font-semibold text-stone-700 dark:text-stone-50">
           Latest Resources
         </h3>
-        <BlogPosts />
+        <div className="mt-4 grid grid-cols-1 gap-6 md:gap-8">
+          {allBlogs
+            .sort((a, b) => {
+              if (new Date(a.metadata.publishedAt) > new Date(b.metadata.publishedAt)) {
+                return -1
+              }
+              return 1
+            })
+            .splice(0, 3)
+            .map(post => {
+              return (
+                <BlogCard
+                  key={post.metadata.title.toLocaleLowerCase().replace(' ', '-')}
+                  name={post.metadata.title}
+                  href={'/blog/' + post.slug}
+                  date={formatDate(post.metadata.publishedAt)}
+                  imgUrl={post.metadata.image}
+                  views={199}
+                />
+              )
+            })}
+        </div>
       </section>
       <section>
         <h3 className="text-md mb-6 font-semibold text-stone-700 dark:text-stone-50"></h3>
