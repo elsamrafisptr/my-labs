b05d810467ef5a6a2f917b796ee7b2ca5f0f59de
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2025-10-08 18:08:50 +0700

    fix: next config for public images

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ import withBundleAnalyzer from '@next/bundle-analyzer'
 
 const baseConfig: NextConfig = {
   output: 'export',
+  basePath: '/my-labs',
   reactStrictMode: true,
   poweredByHeader: false,
   transpilePackages: ['next-mdx-remote'],
