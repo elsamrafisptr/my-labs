ca1053cb488922773072ab3b8743b4efd2776ebf
Elsam Rafi Saputra <134671598+elsamrafisptr@users.noreply.github.com>
2025-10-08 18:19:41 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import withBundleAnalyzer from '@next/bundle-analyzer'
 
 const baseConfig: NextConfig = {
   output: 'export',
-  basePath: '/my-labs',
+  assetPrefix: '/my-labs',
   reactStrictMode: true,
   poweredByHeader: false,
   transpilePackages: ['next-mdx-remote'],
